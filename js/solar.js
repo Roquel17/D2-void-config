432203a3bb7f3314219d1c6a750929fddf41143c
Roquel17 <70388332+Roquel17@users.noreply.github.com>
2020-09-21 18:59:11 -0600

    Add files via upload

---
+++ js/solar.js
@@ -0,0 +1,4 @@
+$(`#rowOne, #rowTwo, #rowThree, #rowFour, #rowFive, #rowSix, #rowSeven, #rowEight, #rowNine, #rowTen, #rowEleven, #rowTwelve, #rowThirteen, #rowFourteen, #rowFifteen, #rowSixteen, #rowSeventeen,#rowEighteen,#rowNineteen,#rowTwenty,#rowTwentyOne,#rowTwentyTwo,#rowTwentyThree,#rowTwentyFour,#rowTwentyFive,#rowTwentySix,#rowTwentySeven,#rowTwentyEight,#rowTwentyNine,#rowThirty,#rowThirtyOne,#rowThirtyTwo,#rowThirtyThree,#rowThirtyFour,#rowThirtyFive,#rowThirtySix,#rowThirtySeven,#rowThirtyEight,#rowThirtyNine,#rowFourty,#rowFourtyOne,#rowFourtyTwo,#rowFourtyThree,#rowFourtyFour,#rowFourtyFive,#rowFourtySix,#rowFourtySeven,#rowFourtyEight,#rowFourtyNine`).click(function() {
+  $(this).toggleClass("checked");
+
+});
